7336064ce4f263202d786be10f716e4007e9ea74
Josaiah Saldana <131936216+JosaiahS@users.noreply.github.com>
2024-08-14 00:00:03 -0400

    Update test_backend.py

--- Back-End/test_backend.py
+++ Back-End/test_backend.py
@@ -1,14 +1,12 @@
-#from  mainreimagined import app  # assuming your main Flask application file is named 'app.py'
-#import os
 
 def test_upload_and_process():
     tester = app.test_client()
     with open('testingtesting.docx', 'rb') as f:
         response = tester.post('/upload', 
                                content_type='multipart/form-data',
-                               data={'file': (f, 'sample.docx')})  # replace with your actual file name
+                               data={'file': (f, 'sample.docx')})  
 
-    print(response.data)  # This will print the response from the server
+    print(response.data)  
 
 if __name__ == "__main__":
     test_upload_and_process()
